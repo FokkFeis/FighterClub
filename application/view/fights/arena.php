d1308952b04c72afe9e47b06947d314ad54df9f0
Stefán Freyr <stebbieff@gmail.com>
2017-05-03 17:20:40 +0000

    Arena overhaul done

--- application/view/fights/arena.php
+++ application/view/fights/arena.php
@@ -1,25 +1,28 @@
-<form method="post" action="#">
-    <div class="col-md-10">
-        <div class="col-md-3 form-group">
-            <select class="form-control" id="select1">
-              <?php foreach ($allFighters as $fighters) {
-                 echo "<option value=" . $fighters->FighterName .">". $fighters->FighterName . "</option>";}
-              ?>
-            </select>
-            <div class="fighter" id="fighter1"></div>
-        </div>
-        <div class="col-md-1">
-            <span> VS </span>
-        </div>
-        <div class="col-md-3 form-group">
-            <select class="form-control" id="select2">
-              <?php
-               foreach ($allFighters as $fighters) {
-                 echo "<option value =" . $fighters->FighterName .">". $fighters->FighterName . "</option>";}
-                 ?>
-            </select>
-          <div class="fighter" id="fighter2"></div>
-        </div>
+<div class="card-deck">
+  <div class="card">
+    <div class = "card-block">
+      <select class="form-control card-title" id="select1">
+        <?php foreach ($allFighters as $fighters) {
+          echo "<option value=" . $fighters->FighterName .">". $fighters->FighterName . "</option>";}
+          ?>
+      </select>
+      <ul class="list-group list-group-flush" id="fighter1"></ul>
     </div>
-    <input type="submit" value="Fight!" />
-</form>
+  </div>
+
+  <div class="card">
+      <p class="card-text display-1 text-center">VS</p>
+  </div>
+
+  <div class="card">
+    <div class = "card-block">
+      <select class="form-control card-title" id="select2">
+        <?php foreach ($allFighters as $fighters) {
+          echo "<option value=" . $fighters->FighterName .">". $fighters->FighterName . "</option>";}
+          ?>
+      </select>
+      <ul class="list-group list-group-flush" id="fighter2"></ul>
+    </div>
+  </div>
+</div>
+<input type="submit" value="Fight!" />
