308044361300ca17819786b9c76205f85731974b
Stefán Freyr <stebbieff@gmail.com>
2017-05-03 16:30:51 +0000

    Final version of fighters overhaul

--- application/view/fights/fighters.php
+++ application/view/fights/fighters.php
@@ -1,9 +1,9 @@
 
-<div class="card-group">
+<div class="card-columns">
   <?php foreach($allFighters as $fighters) {?>
     <div class="card">
       <div class = "card-block">
-        <h4 class="card-title"><?php echo $fighters->FighterName ?></h4>
+        <h5 class="card-title"><?php echo $fighters->FighterName ?></h5>
         <ul class="list-group list-group-flush">
           <li class="list-group-item">Strength: <?php echo $fighters->strength ?></li>
           <li class="list-group-item">Wins: <?php echo $fighters->wins ?></li>
