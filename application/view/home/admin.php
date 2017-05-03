fd832e1d634aa3003bd2bb292a42d8e633e1f229
Stefán Freyr <stebbieff@gmail.com>
2017-05-03 16:10:23 +0000

    Changed from nav to btn-group for admin panel

--- application/view/home/admin.php
+++ application/view/home/admin.php
@@ -1,7 +1,6 @@
 <?php
 # make some user info stuff here
 # logout button needs to be added
-# in the nav for admin functions we need the  onClick="return false;" so that the page doesn't reload or go to top.
 if(!isset($_SESSION['username']) && $_SESSION['isAdmin'] != '1')
 {
   header('Location: ' . URL . 'login');
@@ -9,18 +8,17 @@ if(!isset($_SESSION['username']) && $_SESSION['isAdmin'] != '1')
   $userEnc = json_encode($data, true);
   $userDec = json_decode($userEnc, true);
 ?>
-<nav class="navbar">
-  <ul class="nav nav-pills nav-justified">
-    <li class="nav-item">
-      <a class="nav-link" href="#" id="allUsersButton" onClick="return false;">Show all users</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="" id="allBetsButton" onClick="return false;">Show all Bets</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="" id="addFighterButton" onClick="return false;">Add fighter</a>
-    </li>
-  </ul>
-</nav>
 
+<div class="btn-group btn-group-justified" role="group">
+  <div class="btn-group" role="group">
+      <button type="button" class="btn btn-secondary" id="allUsersButton">Show all users</button>
+  </div>
+  <div class="btn-group" role="group">
+      <button type="button" class="btn btn-secondary" id="allBetsButton">Show all bets</button>
+  </div>
+  <div class="btn-group" role="group">
+      <button type="button" class="btn btn-secondary" id="addFighterButton">Add fighters</button>
+  </div>
+</div>
+<hr>
 <div class = "col-md-10" id="displayBox"><div>
