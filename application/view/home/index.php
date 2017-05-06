1d76a7e10a03955a732f257996126704b50c64c8
Stefán Freyr <stebbieff@gmail.com>
2017-05-06 17:23:22 +0000

    HTML/CSS first draft done on home/index

---
+++ application/view/home/index.php
@@ -0,0 +1,38 @@
+<div class="container">
+  <div class="row front-page">
+    <h1>Welcome to Fighter Club</h1>
+    <p><strong>This website allows it's users to bet on fake fights with virtual currency.</strong> Once you've
+    <a href="#">signed up</a> you will be given 50 credits to bet in the arena. You will be able to choose what
+    fighters you want to fight and which one you want to bet on. The fight is determined by 3 dice rolls and will factor in the fighters strength.
+     So if you choose fighters with a high difference in strength the chances of the underdog winning are much lower.
+      We factor that in to our algorithm so the payout will be higher if you bet on the underdog.
+       Once the fight is over your balance will change depending if you win or loose.</p>
+    <div class="col-md-5">
+      <h3 class="middle-align">Leagues and minimum bets</h3>
+      <ul class="">
+        <li><strong>Bronze</strong> Minimum bet 1 Maximum 25</li>
+        <li><strong>Silver</strong> Minimum bet 25 maximum 50</li>
+        <li><strong>Gold</strong> Minimum bet 50 maximum 100</li>
+        <li><strong>Masters</strong> Minimum bet 100 no maximum</li>
+        <li><strong>Grand Master</strong> Minimum bet 500 / ALL IN</li>
+      </ul>
+    </div>
+    <div class="col-md-5">
+      <h3 class="middle-align">Information Center</h3>
+      <ul class="">
+        <li><strong>Fighters</strong> Detailed information about every fighter, filterable by league</li>
+        <li><strong>Fighter leaderboards</strong> Showcases the top 10 fighters and their stats</li>
+        <li><strong>My stats</strong> Shows your betting history and gains/losses</li>
+      </ul>
+    </div>
+
+
+    <blockquote class="blockquote col-md-12">
+      <p class="mb-0">The first rule of Fight Club is: You do not talk about Fight Club <br>The second rule of Fight Club is:
+    You do not talk about Fight Club</p>
+      <footer class="blockquote-footer">Brad Pitt as Tyler Durden in
+        <cite title="Source Title">Fight Club</cite>
+      </footer>
+    </blockquote>
+  </div>
+</div>
